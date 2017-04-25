14670a01d29978238ca6425e81620c05d374d1d4
v1kttor <viktoras@bmx.lt>
2017-04-25 13:12:31 +0300

    if and else statements

--- static/src/js/daterange.js
+++ static/src/js/daterange.js
@@ -4,33 +4,48 @@ $(function ()
   var start = moment().subtract(29, 'days');
   var end = moment();
 
-  function cb(start, end)
-  {
-  $('#reportrange').on('change', 'input', function()
-  {
-  var first = start.format('YYYY-MM-DD');
-  var last = end.format('YYYY-MM-DD');
-  dates_to_url(first, last);
-  });
-  }
-// paimti parametra is url ir paduot ji i start ir i end date
-//https://momentjs.com/docs/#/parsing/
-//var day = moment("1995-12-25");
+    function cb(start, end)
+    {
+    $('#reportrange').on('change', 'input', function()
+    {
+    var first = start.format('YYYY-MM-DD');
+    var last = end.format('YYYY-MM-DD');
+    dates_to_url(first, last);
+    });
+    }
 
-  function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
-  }
-  start_date = getURLParameter('start');
-  end_date = getURLParameter('end');
-  console.log(start_date, end_date);
+    // if (location.pathname == '/my/my_timesheets_date/') {
+    //   var start = moment().subtract(29, 'days');
+    //   var end = moment();
+    // }
+    // else {
+    //   function getURLParameter(name) {
+    //   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+    //   }
+    //   start_date = getURLParameter('start');
+    //   end_date = getURLParameter('end');
+    //   var start = moment(start_date);
+    //   var end = moment(end_date);
+    //
+    // }
 
-  var start = moment(start_date);
-  var end = moment(end_date);
 
-  function dates_to_url(first, last)
-  {
-  window.location = "/my/my_timesheets_date/?start=" + first + "&end=" + last;
-  }
+    if (location.pathname != '/my/my_timesheets_date/') {
+      function getURLParameter(name) {
+      return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+      }
+      start_date = getURLParameter('start');
+      end_date = getURLParameter('end');
+      var start = moment(start_date);
+      var end = moment(end_date);
+    }
+
+
+    function dates_to_url(first, last)
+    {
+    window.location = "/my/my_timesheets_date/?start=" + first + "&end=" + last;
+    }
+
      $('#reportrange input').daterangepicker({
         startDate: start,
         endDate: end,
