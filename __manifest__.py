873ab6086fa5be41970cba28f2556e395e82e987
Viktoras Norkus <viktoras@versada.eu>
2017-06-14 10:10:51 +0300

    minor changes

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "website_timesheets_date",
-    'summary': 'website_timesheets_date',
-    'description': 'website_timesheets_date',
-    'author': "Viktoras",
-    'website': "http://www.yourcompany.com",
-    'category': 'Uncategorized',
+    'name': "Website Timesheets",
+    'summary': 'Website Timesheets with datepicker',
+    'author': "Viktoras Norkus",
+    'category': 'Website',
     'version': '10.0.0.1',
     'depends': ['base', 'invoice_time_sheet_lines'],
     'data': ['views/templates.xml'],
